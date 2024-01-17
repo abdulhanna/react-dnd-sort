4a3907dac0de595b39c9501772805b23884cb49c
abdul hannan <abdul.hannan@plaxonic.com>
2024-01-17 18:53:00 +0530

    Bump version to x.y.z

--- src/component.jsx
+++ src/component.jsx
@@ -3,76 +3,73 @@ import React, { useState,useCallback, useRef } from 'react';
 import { DndProvider, useDrag, useDrop } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import ListComponent from './list';
-
-
-
-
-
+import draggableItemClass from "./draggableItem.css"
 
 const ItemType = 'ITEM';
 
-//Draggable component 
+
 const DraggableItem = ({ id, index, item, moveItem, children }) => {
-    const ref = useRef(null);
-  
-    const [{ handlerId }, drop] = useDrop({
-      accept: ItemType,
-      collect(monitor) {
-        return {
-          handlerId: monitor.getHandlerId(),
-        };
-      },
-      hover(draggedItem,monitor) {
-        if (!ref.current) {
-          return;
-        }
-        const dragIndex = draggedItem.index;
-        const hoverIndex = index;
-  
-        if (dragIndex === hoverIndex) {
-          return;
-        }
-  
-        // Determine the rectangle on screen
-        const hoverBoundingRect = ref.current.getBoundingClientRect();
-        const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-        const clientOffset = monitor.getClientOffset();
-        const hoverClientY = clientOffset.y - hoverBoundingRect.top;
-  
-        if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-          return;
-        }
-  
-        if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-          return;
-        }
-  
-        moveItem(dragIndex, hoverIndex);
-        draggedItem.index = hoverIndex;
-      },
-    });
-  
-    const [{ isDragging }, drag] = useDrag({
-      type: ItemType,
-      item: () => {
-        return { id, index };
-      },
-      collect: (monitor) => ({
-        isDragging: monitor.isDragging(),
-      }),
-    });
-  
-    const opacity = isDragging ? 0.5 : 1;
-    const boxShadow = isDragging ? "0 4px 8px rgba(0, 0, 0, 0.3)" : "none";
-    // const border = isDragging ? "2px dashed gray" : "2px solid gray";
-    drag(drop(ref));
-  
-    return (
-      <div ref={ref} style={{ padding: '8px', marginBottom: '4px',  backgroundColor: "white", opacity,boxShadow }} data-handler-id={handlerId}>
-        {React.cloneElement(children, { draggable: false })} {/* Disable dragging on the child component */}
-      </div>
-    );
-  };
+  const ref = useRef(null);
+
+  const [{ handlerId }, drop] = useDrop({
+    accept: ItemType,
+    collect(monitor) {
+      return {
+        handlerId: monitor.getHandlerId(),
+      };
+    },
+    hover(draggedItem,monitor) {
+      if (!ref.current) {
+        return;
+      }
+      const dragIndex = draggedItem.index;
+      const hoverIndex = index;
+
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+
+      // Determine the rectangle on screen
+      const hoverBoundingRect = ref.current.getBoundingClientRect();
+      const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+      const clientOffset = monitor.getClientOffset();
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return;
+      }
+
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return;
+      }
+
+      moveItem(dragIndex, hoverIndex);
+      draggedItem.index = hoverIndex;
+    },
+  });
+
+  const [{ isDragging }, drag] = useDrag({
+    type: ItemType,
+    item: () => {
+      return { id, index };
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
+  // const opacity = isDragging ? 0.5 : 1;
+  // const boxShadow = isDragging ? "0 4px 8px rgba(0, 0, 0, 0.3)" : "none";
+  // const border = isDragging ? "2px dashed gray" : "2px solid gray";
+  drag(drop(ref));
+  const itemClass = `${draggableItemClass} ${boxShadow} ${opacity}}`;
+  return (
+    <div ref={ref} style={itemClass} data-handler-id={handlerId}>
+      {React.cloneElement(children, { draggable: false })} {/* Disable dragging on the child component */}
+    </div>
+  );
+};
+
 
 
   //Component for Sorting Item 
